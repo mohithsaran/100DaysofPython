46d65c19469f8af025a22f09aae5a4b073b7d85c
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-25 18:09:50 +0530

    Day 6 Datatypes and variables

---
+++ Day6/app.py
@@ -0,0 +1,27 @@
+a=1
+print(type(a))
+b=1.234
+print(type(b))
+c=a+b
+print(type(c))
+d="Mohith"
+print(type(d))
+a1="1"
+print(type(a1))
+comp=complex(8,2)
+print(type(comp))
+print(comp)
+c=True
+print(type(c))
+list1=[1,4,"apple",[1,2,3],["parrot"]] #mutable
+print(list1)
+print(type(list1))
+tu=("parrot","sparrow","lion",(1,2,3),1.1) #immutable
+print(tu)
+print(type(tu))
+dict1={
+  "name":"mohith",
+  "age":20
+}
+print(dict1)
+print(type(dict1))
