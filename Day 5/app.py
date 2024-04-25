658b99083d5da7c1c88852307d81692225423b6a
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-25 15:29:07 +0530

    Day 5 Comments Escape Sequences and print statement with separator and end

---
+++ Day 5/app.py
@@ -0,0 +1,15 @@
+#Day 5
+#Author:Mohith
+#Course:100Days of Python
+print('Day 5')
+print('Hi Iam learning from \n Code with Harry')
+#this is single line comment
+'''This is 
+multi line comment
+'''
+
+# print("This doesnt "execute")
+print("with \"double quote\"")
+
+print("Hey",6,7,sep="-",end="009\n")
+print('Mohith')
