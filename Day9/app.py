29d57df0777a3b0a9742a1f739864c4411273a1a
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-26 09:43:12 +0530

    Day 9 Data type conversions implictly and explicity

---
+++ Day9/app.py
@@ -0,0 +1,22 @@
+a = "1"
+# a = 1
+b = "2"
+# b = 2
+print(int(a) + int(b))
+
+string = "15"
+number = 7
+string_number = int(string)  #throws an error if the string is not a valid integer
+sum= number + string_number
+print("The Sum of both the numbers is: ", sum)
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+
+print(c + d) #converted to float
+
+p="7"
+q=5
+# print(p+q) #throws an error cant convert string to int so p must be converted to int int(p)
+
