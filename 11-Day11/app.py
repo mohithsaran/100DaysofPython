cd071367dd5be09bc1e8cd1e9cfd1c5817671b96
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-26 15:56:25 +0530

    Day 11 Introduction to string

---
+++ 11-Day11/app.py
@@ -0,0 +1,22 @@
+
+name = "Mohith"
+friend = 'Saran'
+print("Hello, " + name)
+
+a = """
+Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.
+"""
+print(a)
+
+print(name[0])
+print(name[-1])
+# print(name[6]) throws indexerror
+
+for character in name:
+  print(character)
+
+for character in a:
+  print(character)
