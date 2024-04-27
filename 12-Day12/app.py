b44cfb9b2ec2ead512fd9015f0f1bd695a35925e
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-27 17:34:26 +0530

    Day 12 String slicing and methods

---
+++ 12-Day12/app.py
@@ -0,0 +1,15 @@
+name="Mohith,Saran"
+print(name[0:])
+print(len(name))
+print(name[0:6])
+fruit="mango"
+mangolen=len(fruit)
+print(mangolen)
+print(fruit[0:4])
+print(fruit[:4])
+print(fruit[1:4])
+print(fruit[:])
+print(fruit[:-1])
+
+for i in fruit:
+  print(i)
