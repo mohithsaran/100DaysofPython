0fd813697a79b5c23fe564d665e0d3afcebce2db
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-04-29 21:43:44 +0530

    Day 14 if, if else, elif, nested-if are added

---
+++ 14-Day14/elif.py
@@ -0,0 +1,10 @@
+
+num = int(input('Enter the number: '))
+if (num < 0):
+    print("Number is negative.")
+elif (num == 0):
+    print("Number is Zero.")
+else:
+    print("Number is positive.")
+
+print('The number is real number')
---
+++ 14-Day14/if.py
@@ -0,0 +1,22 @@
+a = int(input("Enter your age: "))
+print("Your age is:", a)
+# Conditional operators
+# >, <, >=, <=, ==, !=
+# print(a>18)
+# print(a<=18)
+# print(a==18)
+# print(a!=18)
+if(a>18):
+  print("You can drive")
+  print("Yes")
+else:
+  print("You cannot drive")
+  print("No")
+  print("Yay!")
+
+applePrice = 210
+budget = 200
+if (applePrice <= budget):
+    print("Alexa, add 1 kg Apples to the cart.")
+else:
+    print("Alexa, do not add Apples to the cart.")
