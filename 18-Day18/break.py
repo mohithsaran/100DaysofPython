f1b4f79ebd018fbb4d97b6c83658ad44bb7b8e7f
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-05-01 15:36:12 +0530

    Day 18 break and continue

---
+++ 18-Day18/break.py
@@ -0,0 +1,6 @@
+for i in range(1, 13):
+    if (i == 11):
+        break
+    print("5 X", i, " = ", 5 * i)
+
+print('Loop broken and came out of the loop')
---
+++ 18-Day18/continue.py
@@ -0,0 +1,6 @@
+for i in range(1, 13):
+    if (i == 11):
+        continue
+    print("5 X", i, " = ", 5 * i)
+
+
