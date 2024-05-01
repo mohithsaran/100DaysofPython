14f08396a675f201e7d36b0f9e784437e238f1f0
mohithsaran <mohithsaran.kanamarlapudi@gmail.com>
2024-05-01 17:19:06 +0530

    Day 19 functions

---
+++ 19-Day19/functions.py
@@ -0,0 +1,24 @@
+def calcGmean(a, b):
+    mean = (a * b) / (a + b)
+    print(mean)
+
+
+def isGreater(a, b):
+    if (a > b):
+        print(a, " is greater than ", b)
+    else:
+        print(a, " is greater than ", b)
+
+
+a = 9
+b = 8
+calcGmean(a, b)
+isGreater(a, b)
+
+c = 8
+d = 7
+calcGmean(c, d)
+isGreater(c, d)
+
+
+
